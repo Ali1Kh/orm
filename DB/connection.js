6bbb1013e56e66222108b29fec356bf38ab50cf9
Ali Khaled <alielsaadany94@gmail.com>
2023-12-06 21:00:43 +0200

    sql live

--- DB/connection.js
+++ DB/connection.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-export const sequelize = new Sequelize("assignments", "root", "root", {
-  host: "localhost",
+export const sequelize = new Sequelize("sql8668289", "sql8668289", "VFiS4Heu5H", {
+  host: "sql8.freesqldatabase.com",
   dialect: "mysql",
 });
 
